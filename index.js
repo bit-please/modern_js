10e619442f45b16b6a78330ca7d6ca39bbe52827
danizaghian <dani.zaghian@gmail.com>
2019-06-11 09:38:10 -0700

    adds momentjs library

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var message = "Hello from JavaScript!"
 console.log(message);
-document.querySelector("p").innerText = message;
+
+var datetime = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(datetime);
