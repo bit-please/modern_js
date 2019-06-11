c26806fe3a9b427b116e7efc44a89a932fb47af0
danizaghian <dani.zaghian@gmail.com>
2019-06-11 10:40:19 -0700

    adds gitignore to ignore node modules, adds webpack

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+var moment = require('moment');
+var math = require('mathjs');
+
 var message = "Hello from JavaScript!"
 console.log(message);
 
 var datetime = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(datetime);
+
+console.log(math.sqrt(-4));
