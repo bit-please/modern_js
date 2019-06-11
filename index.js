30bf27b605e9136b3f7348b7d7a678b994920b95
danizaghian <dani.zaghian@gmail.com>
2019-06-11 11:28:55 -0700

    adds webpack configuration and babel

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var moment = require('moment');
+import moment from 'moment';
 var math = require('mathjs');
 
-var message = "Hello from JavaScript!"
-console.log(message);
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
 
 var datetime = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(datetime);
